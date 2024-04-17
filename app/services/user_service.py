37518775af9f6c1096784f736a99d9b85210f841
whua0 <wh44@njit.edu>
2024-04-17 12:49:13 -0400

    Fixed user_services.py UserServices.update, profile_picture_url converted to string before interacting with db

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -70,6 +70,9 @@ class UserService:
 
             if 'password' in validated_data:
                 validated_data['hashed_password'] = hash_password(validated_data.pop('password'))
+
+            validated_data['profile_picture_url'] = str(validated_data['profile_picture_url'])
+
             query = update(User).where(User.id == user_id).values(**validated_data).execution_options(synchronize_session="fetch")
             await cls._execute_query(session, query)
             updated_user = await cls.get_by_id(session, user_id)
