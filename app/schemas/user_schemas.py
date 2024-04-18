5cf9fcf82875f6a5540c842018cf16797af070ce
russf19 <russ_f19@verizon.net>
2024-04-17 20:33:04 -0400

    made fixes to user_schema

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -142,8 +142,8 @@ class UserCreate(UserBase):
                 "email": "john.doe@example.com",
                 "password": "SecurePassword123!",
                 "full_name": "John Doe",
-                "bio": "I am a data scientist passionate about machine learning and big data analytics.",
-                "profile_picture_url": "https://example.com/profile_pictures/jane_smith.jpg"
+                "bio": "I am a software engineer with over 5 years of experience in building scalable web applications using Python and JavaScript.",
+                "profile_picture_url": "https://example.com/profile_pictures/john_doe.jpg"
             }
         }
 
