4b32c2840e8d754f6f9356bec189f8951c9e3636
TyHys <tyler.hayes@live.com>
2024-04-18 21:29:53 -0400

    Added email already registered message response

--- app/routers/user_routes.py
+++ app/routers/user_routes.py
@@ -130,6 +130,10 @@ async def create_user(user: UserCreate, request: Request, db: AsyncSession = Dep
     if existing_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists")
     
+    existing_email = await UserService.get_by_email(db, user.email)
+    if existing_email:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
+    
     created_user = await UserService.create(db, user.model_dump())
     if not created_user:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to create user")
